fa2a3c9a945534d64f126a4cd96e438df7163c6e
unknown <eloso85@gmail.com>
2022-06-27 23:30:58 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port, console.log(`server running on port ${port}`);)
+app.listen(port, console.log(`server running on port ${port}`));
