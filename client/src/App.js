d8ca84fd842f8c1ace00f8ee00a99dfaf9ef817c
unknown <eloso85@gmail.com>
2022-07-10 13:49:57 -0500

    added client query to frot end

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import Header from './components/Header';
+//apollo provider
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+import Clients from './components/Clients'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
+    </>
   );
 }
 
